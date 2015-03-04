9aad049b8e82495555be8493633af61d2facd668
satishavvaru <sateesh.avvaru@gmail.com>
2015-03-04 09:53:35 +0530

    IdentityDataBaseFirst
    Identity using Database first approach

---
+++ IdentitySample1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentitySample1.Startup))]
+namespace IdentitySample1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
